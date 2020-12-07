2bb9d622cec6d2546487a1144386ec1c433b1b49
longshilin <583297550@qq.com>
2020-12-07 16:02:20 +0800

    * add test and submodule

---
+++ Assets/Editor/MenuTool.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+public class MenuTool
+{
+    [MenuItem("Test Tool/DirectoryOperator Tool")]
+    public static void DirectoryOperatorTool()
+    {
+        var directoryOperatorTest = new DirectoryOperatorTest();
+        directoryOperatorTest.CreateDirTest();
+        directoryOperatorTest.DeleteDirTest();
+    }
+}
---
+++ Assets/LibraryTest/Common/DirectoryOperatorTest.cs
@@ -0,0 +1,19 @@
+﻿using Longshilin.Common;
+using UnityEngine;
+
+public class DirectoryOperatorTest
+{
+    private string dataPath = Application.dataPath;
+
+    public void CreateDirTest()
+    {
+        DirectoryOperator.CreateDir(dataPath + "/Temp");
+        Debug.Log("create dir!");
+    }
+    
+    public void DeleteDirTest()
+    {
+        DirectoryOperator.DeleteDir(dataPath + "/Temp");
+        Debug.Log("delete dir!");
+    }
+}
