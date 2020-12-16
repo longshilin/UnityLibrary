23e92bdd4308828d0662ade91cad37b5eef949a5
longshilin <583297550@qq.com>
2020-12-17 00:20:21 +0800

    *

---
+++ Assets/LibraryTest/Common/MemoryStreamUtilityTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Longshilin.Common;
+using UnityEngine;
+
+namespace LibraryTest.Common
+{
+    public class MemoryStreamUtilityTest : MonoBehaviour
+    {
+        private void Start()
+        {
+            byte[] arr = null;
+            Item item = new Item() {Id = 1001, Name = "Longshilin"};
+            using (MemoryStreamUtility ms = new MemoryStreamUtility())
+            {
+                ms.WriteInt(item.Id);
+                ms.WriteUTF8String(item.Name);
+
+                arr = ms.ToArray();
+            }
+
+
+            if (arr != null)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Debug.Log(string.Format("123"));
+                }
+            }
+        }
+    }
+
+    public class Item
+    {
+        public int Id;
+        public string Name;
+    }
+}
